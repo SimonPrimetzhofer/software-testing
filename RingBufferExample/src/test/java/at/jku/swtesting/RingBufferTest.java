c80079a0f1f8bc35d3603016def2543fbc7671fb
Daniel Jaburek <k11915134@students.jku.at>
2023-03-16 14:35:48 +0100

    feat: added empty test methods

--- RingBufferExample/src/test/java/at/jku/swtesting/RingBufferTest.java
+++ RingBufferExample/src/test/java/at/jku/swtesting/RingBufferTest.java
@@ -1,5 +1,7 @@
 package at.jku.swtesting;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,4 +12,44 @@ public class RingBufferTest {
 		fail("Not yet implemented");  //TODO: Implement tests
 	}
 
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@AfterEach
+	void tearDown() {
+	}
+
+	@Test
+	void capacity() {
+	}
+
+	@Test
+	void size() {
+	}
+
+	@Test
+	void isEmpty() {
+	}
+
+	@Test
+	void isFull() {
+	}
+
+	@Test
+	void enqueue() {
+	}
+
+	@Test
+	void dequeue() {
+	}
+
+	@Test
+	void peek() {
+	}
+
+	@Test
+	void iterator() {
+	}
 }
