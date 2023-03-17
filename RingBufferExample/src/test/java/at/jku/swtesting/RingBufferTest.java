bcc63084d1db70e1013dc780ad91903388cd369c
Daniel Jaburek <k11915134@students.jku.at>
2023-03-17 20:25:08 +0100

    feat: updated test methods

--- RingBufferExample/src/test/java/at/jku/swtesting/RingBufferTest.java
+++ RingBufferExample/src/test/java/at/jku/swtesting/RingBufferTest.java
@@ -13,8 +13,13 @@ public class RingBufferTest {
 	}
 
 
+	private RingBuffer<Integer> ringBufferInt;
+
+	private final int capacity = 5;
+
 	@BeforeEach
 	void setUp() {
+		ringBufferInt = new RingBuffer<>(capacity);
 	}
 
 	@AfterEach
@@ -23,14 +28,56 @@ public class RingBufferTest {
 
 	@Test
 	void capacity() {
+		// empty buffer length test
+		assertEquals(capacity, ringBufferInt.capacity());
+
+		// filling buffer while checking capacity
+		for (int i = 0; i < capacity + 2; i++) {
+			ringBufferInt.enqueue(i);
+			assertEquals(capacity, ringBufferInt.capacity());
+		}
+
+		// dequeuing buffer while checking capacity
+		for(int i = capacity; i > 0; i--) {
+			ringBufferInt.dequeue();
+			assertEquals(capacity, ringBufferInt.capacity());
+		}
 	}
 
 	@Test
 	void size() {
+		// empty buffer size test
+		assertEquals(0, ringBufferInt.size());
+
+		// filling buffer while checking size
+		for (int i = 0; i < capacity + 2; i++) {
+			ringBufferInt.enqueue(i);
+			assertEquals(i < capacity ? i + 1 : capacity, ringBufferInt.size());
+		}
+
+		// dequeuing buffer while checking size
+		for (int i = capacity; i > 0; i--) {
+			ringBufferInt.dequeue();
+			assertEquals(i - 1, ringBufferInt.size());
+		}
 	}
 
 	@Test
 	void isEmpty() {
+		// empty buffer isEmpty test
+		assertTrue(ringBufferInt.isEmpty());
+
+		// filling buffer while checking isEmpty
+		for (int i = 0; i < capacity + 2; i++) {
+			ringBufferInt.enqueue(i);
+			assertFalse(ringBufferInt.isEmpty());
+		}
+
+		// dequeuing buffer while checking isEmpty
+		for (int i = capacity; i > 0; i--) {
+			ringBufferInt.dequeue();
+			assertEquals(i == 1, ringBufferInt.isEmpty());
+		}
 	}
 
 	@Test
