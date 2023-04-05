6b8d5f42e753b291b96f45cc07fa590bd8943e57
Simon Primetzhofer <simon.primetzhofer@live.at>
2023-04-05 12:36:04 +0200

    start mock exercise

--- HttpClientExample/src/test/java/at/jku/swtesting/HttpClientTest.java
+++ HttpClientExample/src/test/java/at/jku/swtesting/HttpClientTest.java
@@ -10,16 +10,20 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class HttpClientTest {
-
 	@Mock
 	private URL mockURL;
+	@Mock private HttpURLConnection mockURLConnection;
+  	@Mock private InputStream mockInputStream;
 
 	@Test
 	public void testGetContentOnline() throws MalformedURLException {
@@ -31,6 +35,17 @@ public class HttpClientTest {
 	@Test
 	public void testGetContentMocked() throws IOException {
 		// TODO: Implement test using mocks
+		HttpClient myClient = new HttpClient();
+
+		when(mockURL.openConnection()).thenReturn(mockURLConnection);
+
+		// mock input stream by creating a mockinputstream class which extends InputStream and returns fixed characters
+		when(mockURLConnection.getInputStream()).thenReturn(mockInputStream);
+		when (mockInputStream.read()).thenReturn(-1);
+		when(myClient.getContent(mockURL)).thenReturn("Mock http response");
+
+		String page = myClient.getContent(mockURL);
+		assertTrue(page.contains("Mock http response"));
 	}
 
 }
