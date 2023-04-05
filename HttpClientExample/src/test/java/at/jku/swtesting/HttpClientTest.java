c5499dcf6468f903ea7f5755e4ebeb6b4c61e451
Simon Primetzhofer <simon.primetzhofer@live.at>
2023-04-05 12:42:35 +0200

    mock InputStream

--- HttpClientExample/src/test/java/at/jku/swtesting/HttpClientTest.java
+++ HttpClientExample/src/test/java/at/jku/swtesting/HttpClientTest.java
@@ -1,29 +1,26 @@
 package at.jku.swtesting;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class HttpClientTest {
+	private static final String TEST_CONTENT = "Http mock response";
+
 	@Mock
 	private URL mockURL;
 	@Mock private HttpURLConnection mockURLConnection;
-  	@Mock private InputStream mockInputStream;
 
 	@Test
 	public void testGetContentOnline() throws MalformedURLException {
@@ -34,18 +31,18 @@ public class HttpClientTest {
 
 	@Test
 	public void testGetContentMocked() throws IOException {
-		// TODO: Implement test using mocks
 		HttpClient myClient = new HttpClient();
 
+		// use mock url connection to not actually connect anywhere in the internet
 		when(mockURL.openConnection()).thenReturn(mockURLConnection);
+		// return defined TEST_CONTENT string in InputStream
+		when(mockURLConnection.getInputStream()).thenReturn(new ByteArrayInputStream(TEST_CONTENT.getBytes()));
 
-		// mock input stream by creating a mockinputstream class which extends InputStream and returns fixed characters
-		when(mockURLConnection.getInputStream()).thenReturn(mockInputStream);
-		when (mockInputStream.read()).thenReturn(-1);
-		when(myClient.getContent(mockURL)).thenReturn("Mock http response");
-
+		// get mocked content
 		String page = myClient.getContent(mockURL);
-		assertTrue(page.contains("Mock http response"));
+
+		// check if HttpClient actually returns TEST_CONTENT
+		assertTrue(page.contains(TEST_CONTENT));
 	}
 
 }
